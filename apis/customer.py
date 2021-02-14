42bca02ca338516b03ad9131ffc57d09ae5ecadf
ozhen <ozhen@sfu.ca>
2021-02-14 13:13:27 -0800

    add get_rental-info

--- apis/customer.py
+++ apis/customer.py
@@ -20,44 +20,56 @@ def get_all():
 
     return customer_list
 
-def getCustomerMovieInfo(first_name, last_name):
-    rental_info = {}
+def get_rental_info(first_name, last_name):
+    rental_list = []
 
+    # search for selected customer's rental info
     for customer in customer_colln.find({'First Name': first_name,
                                          'Last Name': last_name}):
-        rental_info = customer['Rentals']
+        rentals = customer['Rentals']
 
-    print(len(rental_info))
-
-    for movie in rental_info:
-        print(movie)
-        print(movie['Film Title'])
-        rental_date_str = movie['Rental Date']
+    for rental in rentals:
+        rental_date_str = rental['Rental Date']
         rental_date_obj = datetime.datetime.strptime(rental_date_str, '%Y-%m-%d %H:%M:%S.%f')
-        print(rental_date_obj.date())
 
-        return_date_str = movie['Return Date']
-        return_date_obj = datetime.datetime.strptime(return_date_str, '%Y-%m-%d %H:%M:%S.%f')
-        print(return_date_obj.date())
+        return_date_str = rental['Return Date']
 
-        # round
-        print(movie['Payments'][0]['Amount'])
-        break
+        # checks if the movie has been returned
+        if return_date_str:
+            # when movie has returned
+            return_date_obj = datetime.datetime.strptime(return_date_str, '%Y-%m-%d %H:%M:%S.%f')
+            rented_days = return_date_obj.date() - rental_date_obj.date()
+        else:
+            today = datetime.date.today()
+            rented_days = today - rental_date_obj.date()
+
+        rental_info = {"rented_days": rented_days.days}
+
+        # round cost to 2 decimals
+        rental_info["cost"] = round(rental['Payments'][0]['Amount'], 2)
+
+        # add rented movie to the movie list
+        rented_movie = { rental["Film Title"]: rental_info}
+        rental_list.append(rented_movie)
+    
+    return rental_list
+
 
 # complated
 #print(getAll())
+#print(get_rental_info('MARY', 'SMITH'))
 
 # testing
-getCustomerMovieInfo('GRACE', 'ELLIS')
 
 '''
+# helper functions
 def checkPayments():
     for customer in customer_colln.find({}):
         full_name = f"{customer['First Name']} {customer['Last Name']}"
-        rental_info = customer['Rentals']
+        rentals = customer['Rentals']
 
-        for movie in rental_info:
-            if len(movie['Payments']) > 1:
+        for rental in rentals:
+            if len(rental['Payments']) > 1:
                 print(f"{full_name} has more than 1 payments")
 #checkPayments()
 '''
