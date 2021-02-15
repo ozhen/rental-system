250f18d4be829c58a6933298a46c4f258d58bdaa
ozhen <ozhen@sfu.ca>
2021-02-14 16:17:30 -0800

    add comments to film

--- apis/film.py
+++ apis/film.py
@@ -11,6 +11,12 @@ film_colln = db.films
 customer_colln = db.customers
 
 def get_all():
+    """Get all films data from film collection in db
+
+    Returns:
+        list: contain film dicts
+    """
+
     film_list = []
 
     for film in film_colln.find({}):
@@ -28,18 +34,32 @@ def get_all():
 
 
 def get_film(film_name):
-    customer_list = []
+    """Search for the selected film in film collection in db
 
+    Returns:
+        dict: contain film rental histroy and other details
+    """
+    customer_list = []
+    selected_film = {}
+
+    # search the film in film collection
+    for film in film_colln.find({"Title": film_name}):
+        selected_film = film
+
+    # returns when film is not in film collection
+    if not selected_film:
+        return {}
+
+    # search each customer in customer collection
     for customer in customer_colln.find({}):
         rentals = customer['Rentals']
 
+        # search each rental from each customer
         for rental in rentals:
+            # add customer's full name onces the film is found
             if film_name in rental['Film Title']:
                 full_name = f"{customer['First Name']} {customer['Last Name']}"
                 customer_list.append(full_name)
-
-    for film in film_colln.find({"Title": film_name}):
-        selected_film = film
 
     # remove duplicates in customer list
     selected_film["rental_history"] = list(set(customer_list))
@@ -47,3 +67,4 @@ def get_film(film_name):
     return selected_film
 
 print(get_film('FIREBALL PHILADELPHIA'))
+print(get_film('anime'))
