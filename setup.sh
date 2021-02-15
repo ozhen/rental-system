bab5f78dfb5a12b0befd6e45de340b7a271d92d6
ozhen <ozhen@sfu.ca>
2021-02-14 16:38:25 -0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-mongoimport --db dvd-rental-system --collection customers --file DVDRentals-customers.json --authenticationDatabase admin -u root -p rootpassword
+mongoimport --db dvd-rental-system --collection customers --file ./data/DVDRentals-customers.json --authenticationDatabase admin -u root -p rootpassword
 
-mongoimport --db dvd-rental-system --collection films --file DVDRentals-films.json --authenticationDatabase admin -u root -p rootpassword
+mongoimport --db dvd-rental-system --collection films --file ./data/DVDRentals-films.json --authenticationDatabase admin -u root -p rootpassword
